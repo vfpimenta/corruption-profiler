3ec6eda3ed63d2d4761f7c5bc646c04c267a4bbf
Victor Felix Pimenta <vfp@up1c07.windows.cin.ufpe.br>
2017-03-29 14:17:12 -0300

    Updated fetch algorithm Changed functions for fetching data from db. Added timing for functions (new functions improvement of over 5800% in run time). Still need to check if new functions have any side effects in dataset (need plot to confirm).

--- src/handler.py
+++ src/handler.py
@@ -1,40 +1,69 @@
 import sqlite3
+import time
 
-def fetch_expenses(congressperson_id):
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print('{} function took {} ms'.format(f.__name__, (time2-time1)*1000.0))
+        return ret
+    return wrap
+
+@timing
+def fetch_congressman_expenses(congressperson_id):
 	con = sqlite3.Connection('local.db')
 	cur = con.cursor()
 	values = []
 
-	for year in range (2009,2017):
-		for month in range(1,13):
-			cur.execute('SELECT SUM(net_value) FROM previous_years WHERE congressperson_id = ? AND month = ? AND year = ?',[congressperson_id,month,year])
-			value = cur.fetchall()[0][0]
-			if value is None:
-				value = 0
-			values.append(value)
+	for val in cur.execute('SELECT SUM(net_value) FROM previous_years WHERE congressperson_id = ? GROUP BY year, month',[congressperson_id]):
+		tuples.append(val)
+	
+	values = [i[0] for i in tuples]
+	values.append(0)
 
 	cur.close()
 	con.close()
 	return values
 
+@timing
 def fetch_party_expenses(party):
 	con = sqlite3.Connection('local.db')
 	cur = con.cursor()
 	values = []
 
-	for year in range (2009,2017):
-		for month in range(1,13):
-			cur.execute('SELECT SUM(net_value) FROM previous_years WHERE party = ? AND month = ? AND year = ?',[party,month,year])
-			value = cur.fetchall()[0][0]
-			if value is None:
-				value = 0
-			values.append(value)
+	for val in cur.execute('SELECT SUM(net_value) FROM previous_years WHERE party = ? GROUP BY year, month',[party]):
+		tuples.append(val)
+	
+	values = [i[0] for i in tuples]
+	values.append(0)
 
 	cur.close()
 	con.close()
 	return values
 
+@timing
 def fetch_state_expenses(state):
+	con = sqlite3.Connection('local.db')
+	cur = con.cursor()
+	tuples = []
+
+	for val in cur.execute('SELECT SUM(net_value) FROM previous_years WHERE state = ? GROUP BY year, month',[state]):
+		tuples.append(val)
+	
+	values = [i[0] for i in tuples]
+	values.append(0)
+
+	cur.close()
+	con.close()
+	return values
+
+# =================
+#     OLD MODEL
+# =================
+
+@timing
+def old_fetch_state_expenses(state):
 	con = sqlite3.Connection('local.db')
 	cur = con.cursor()
 	values = []
