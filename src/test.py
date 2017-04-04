f884e0655675314fd30fe3249defc22126f13674
vfpimenta <vfpimenta.93@gmail.com>
2017-04-03 22:55:22 -0300

    Ran more tests Added overview for state tests. Added subquota tests.

--- src/test.py
+++ src/test.py
@@ -8,6 +8,7 @@ months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec
 years = ['2009','2010','2011','2012','2013','2014','2015','2016']
 parties = ['DEM','PCdoB','PDT','PEN','PHS','PMB','PMDB','PMN','PP','PPS','PR','PRB','PROS','PRP','PRTB','PSB','PSC','PSD','PSDB','PSDC','PSL','PSOL','PT','PTB','PTC','PTN','PTdoB','PV','REDE','S.PART.','SD']
 states = ['SP','PR','CE','BA','SC','RS','MG','RJ','RO','PB','MS','SE','MA','DF','PI','AL','PE','PA','RR','TO','AC','AP','GO','AM','ES','RN','MT']
+subquota_description = ['Maintenance of office supporting parliamentary activity','Fuels and lubricants','Consultancy, research and technical work','Publicity of parliamentary activity','Security service provided by specialized company','Flight tickets','Telecommunication','Postal services','Lodging, except for congressperson from Distrito Federal','Automotive vehicle renting or watercraft charter','Flight ticket issue','Locomotion, meal and lodging','Purchase of office supplies','Congressperson meal','Publication subscriptions','Automotive vehicle renting or charter','Taxi, toll and parking','Software purchase or renting; Postal services; Subscriptions','Aircraft renting or charter of aircraft','Terrestrial, maritime and fluvial tickets','Watercraft renting or charter','Participation in course, talk or similar event']
 markers = itertools.cycle(('o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X'))
 
 x_ticks = []
