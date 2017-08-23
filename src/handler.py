06210eabe79e7b8a717a9d584acd14aef1330f04
vfpimenta <vfpimenta.93@gmail.com>
2017-08-22 21:46:23 -0300

    Refactored workspace

--- src/handler.py
+++ src/handler.py
@@ -10,7 +10,7 @@ def timing(f):
         return ret
     return wrap
 
-@timing
+#@timing
 def fetch_expenses(field, value):
 	con = sqlite3.Connection('../data/local.db')
 	cur = con.cursor()
@@ -18,6 +18,27 @@ def fetch_expenses(field, value):
 
 	for val in cur.execute('SELECT c.year, c.month, COALESCE(py.net,0) FROM calendar c LEFT JOIN (SELECT year,month,SUM(net_value) as net FROM previous_years WHERE {} = ? GROUP BY year, month) py ON py.year = c.year AND py.month = c.month'.format(field),[value]):
 		tuples.append(val)
+
+	values = [i[2] for i in tuples]
+
+	cur.close()
+	con.close()
+	return values
+
+def fetch_sum(trim):
+	con = sqlite3.Connection('../data/local.db')
+	cur = con.cursor()
+	tuples = []
+
+	sql = 'SELECT c.year, c.month, COALESCE(py.net,0) FROM calendar c LEFT JOIN (SELECT year,month,SUM(net_value) as net FROM previous_years GROUP BY year, month) py ON py.year = c.year AND py.month = c.month'
+	if trim:
+		# jul 2009 ~ ago 2016
+		sql += ' WHERE (c.year = ? AND c.month >= ?) OR (c.year = ? AND c.month <= ?) OR (c.year >= ? AND c.year <= ?)'
+		for val in cur.execute(sql,[2009,7,2016,8,2010,2015]):
+			tuples.append(val)
+	else:
+		for val in cur.execute(sql):
+			tuples.append(val)
 
 	values = [i[2] for i in tuples]
 
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from profiler import Profiler
+
+def main():
+	profiler = Profiler()
+	#profiler.plot_general('party')
+	profiler.plot_sum()
+
+if __name__ == '__main__':
+	main()
