594c674cfa957620c267e00fef20a750bbcaabef
Victor Pimenta <vfpimenta.93@gmail.com>
2018-05-17 00:08:27 -0300

    Added the rest of the expenses data.

--- src/migrator.sh
+++ src/migrator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-expenses=("default" "flight" "fuels" "telecom")
+ 
+expenses=("flight" "publicity" "telecom" "fuels" "maintenance" "consultancy" "auto-watercraft" "auto" "postal" "flight-ticket" "lodging" "meal" "aircraft" "security" "locomotion" "taxi" "publication" "software" "office" "watercraft" "maritme" "course")
 distances=("JS" "cosine" "robust")
 ks=(2 3 4 5)
 legislatures=(53 54 55)
@@ -9,7 +9,7 @@ for expense in ${expenses[@]}; do
 	for distance in ${distances[@]}; do
 		for k in ${ks[@]}; do
 			for legislature in ${legislatures[@]}; do
-				source_file="../data/$expense/graphs/$distance/k-$k/cibm-regioncolor-$legislatures.graphml"
+				source_file="../data/$expense/graphs/$distance/k-$k/cibm-regioncolor-$legislature.graphml"
 				target_path="../../vfpimenta.github.io/data/graph-json/standard/$expense/$distance/k-$k"
 				target_file="cibm-legislature-$legislature.json"
 
--- src/pipeline.sh
+++ src/pipeline.sh
@@ -1,9 +1,12 @@
 echo "[PIPELINE] Starting pipeline"
 echo "[PIPELINE] Building data"
 #Rscript outliers.R -d
-series=$1
+#series=$1
 #./flip.sh
-# Rscript cibm.R -c -g -s $series
+expenses=("maintenance" "consultancy" "auto-watercraft" "auto" "postal" "flight-ticket" "lodging" "meal" "aircraft" "security" "locomotion" "taxi" "publication" "software" "office" "watercraft" "maritme" "course")
+for expense in ${expenses[@]}; do
+  Rscript cibm.R -c -g -s $expense
+done
 # Rscript distance.R -s $series
 # python3 distance.py --series-type=$series
 # python3 main.py -c 4 -d --series-type=$series
@@ -11,9 +14,9 @@ series=$1
 # python3 main.py -c 8 -d --series-type=$series
 # echo "[PIPELINE] Data building finished"
 # sleep 5
-# echo "[PIPELINE] Running analysis"
-python3 mstknn_analysis.py --series-type=$series -k 2,3,4,5 --method=all --function=both -e
-python3 mstknn_analysis.py --series-type=$series -k 4,6,8 --method=kmeans --function=both -e
-python3 mstknn_analysis.py --series-type=$series -k 2,3,4,5 --method=all --function=both -c
-python3 mstknn_analysis.py --series-type=$series -k 4,6,8 --method=kmeans --function=both -c
+# # echo "[PIPELINE] Running analysis"
+# python3 mstknn_analysis.py --series-type=$series -k 2,3,4,5 --method=all --function=both -e
+# python3 mstknn_analysis.py --series-type=$series -k 4,6,8 --method=kmeans --function=both -e
+# python3 mstknn_analysis.py --series-type=$series -k 2,3,4,5 --method=all --function=both -c
+# python3 mstknn_analysis.py --series-type=$series -k 4,6,8 --method=kmeans --function=both -c
 #./flip.sh
